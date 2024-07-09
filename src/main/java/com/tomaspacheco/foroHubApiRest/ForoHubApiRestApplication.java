77bb2de4e674c94104ba935649e0e7708131c2b3
thesoftmoon <tomas.pacheco.c@gmail.com>
2024-07-09 12:48:02 -0400

    initial commit

---
+++ src/main/java/com/tomaspacheco/foroHubApiRest/ForoHubApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.tomaspacheco.foroHubApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomaspacheco/foroHubApiRest/ForoHubApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomaspacheco.foroHubApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
